718101ad09d2587fe44314fee7a46f6febb15a03
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-12-16 11:38:13 +0300

    Написал main (?)

--- src/main.c
+++ src/main.c
@@ -8,7 +8,42 @@
 #include <stdio.h>  /*printf()*/
 #include <stdlib.h> /*EXIT_SUCCESS*/
 
+typedef enum {
+	CORRECT,
+	BAD_PAIR,
+	CROSS_LOOP,
+	ALLOC_ERR
+} CheckResult;
+
+typedef struct {
+	int m, n;
+} Pair;
+
+static CheckResult CheckPairs(Pair const pairs[], int const count)
+{
+	for (int i = 0; i < count; ++i) {
+	
+	}
+}
+
 int main() {
+	Pair const pairs[] = {{1, 5}, {2, 3}, {6, 9}};
+	int const count = (int)(sizeof(pairs) / sizeof(*pairs));
+
+	switch (CheckPairs(pairs, count)) {
+	case CORRECT:
+		puts("Пары корректны\n");
+		break;
+	case BAD_PAIR: 
+		puts("Неправильные пары\n");
+		break;
+	case CROSS_LOOP:
+		puts("Циклы пересекаются\n");
+		break;
+	case ALLOC_ERR:
+		perror("");
+		break;
+	}
 	printf("Hello World!\n");
 	return EXIT_SUCCESS;
 }
